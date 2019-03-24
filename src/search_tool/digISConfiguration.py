34b9742cf7e988813855f7df5a6d7975ac7b7362
Janka <xputer00@stud.fit.vutbr.cz>
2019-03-24 20:25:19 +0100

    updated digIS configuration, negative_db removed, context_size_orf added

--- src/search_tool/digISConfiguration.py
+++ src/search_tool/digISConfiguration.py
@@ -1,5 +1,8 @@
 class digISConfiguration:
-    def __init__(self, isfinder_orf, negative_orf, context_size_orf):
+    def __init__(self, isfinder_orf, context_size_orf, isfinder_is, context_size_is, min_distance):
         self.isfinder_orf = isfinder_orf
-        self.negative_orf = negative_orf
+        self.isfinder_is = isfinder_is
         self.context_size_orf = context_size_orf
+        self.context_size_is = context_size_is
+        self.min_distance = min_distance
+
