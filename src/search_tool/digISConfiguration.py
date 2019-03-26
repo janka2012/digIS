9a83f690d19f449dcff8337f47c4d4e308c985ef
Janka <xputer00@stud.fit.vutbr.cz>
2019-03-26 20:20:53 +0100

    configuration update

--- src/search_tool/digISConfiguration.py
+++ src/search_tool/digISConfiguration.py
@@ -1,8 +1,14 @@
+import os
+from definitions import ROOT_DIR
+
+
 class digISConfiguration:
-    def __init__(self, isfinder_orf, context_size_orf, isfinder_is, context_size_is, min_distance):
-        self.isfinder_orf = isfinder_orf
-        self.isfinder_is = isfinder_is
+    def __init__(self, context_size_orf, context_size_is, max_merge_distance, min_gb_overlap):
+        self.models = os.path.join(ROOT_DIR, "data", "models", "hmm", "hmm_all_fams.hmm")
+        self.isfinder_orf_db = os.path.join(ROOT_DIR, "data", "Blast_db", "isfinder_orf")
+        self.isfinder_is_db = os.path.join(ROOT_DIR, "data", "Blast_db", "isfinder_is")
         self.context_size_orf = context_size_orf
         self.context_size_is = context_size_is
-        self.min_distance = min_distance
+        self.max_merge_distance = max_merge_distance
+        self.min_gb_overlap = min_gb_overlap
 
