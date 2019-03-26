c09ae5ad8d053adc9371c1e0e417bde1cf597ec3
Janka <xputer00@stud.fit.vutbr.cz>
2019-03-26 20:24:27 +0100

    hard coded values moved to config, refactoring

--- src/search_tool/digIS.py
+++ src/search_tool/digIS.py
@@ -1,36 +1,39 @@
-import tempfile
-
 from copy import copy
 from copy import deepcopy
 
-
+from src.blast.Blast import Blast
 from src.blast.BlastX import BlastX
+from src.blast.BlastN import BlastN
 from src.common.csv_utils import write_csv
-from src.common.misc import get_best_blast_hits_in_range, delete_file
 from src.common.sequence import *
+from src.common.genbank import read_gb
 from src.search_tool.RecordDigIS import RecordDigIS
+from src.search_tool.digISClassifier import digISClassifier
+from src.genbank.RecordGenbank import RecordGenbank
 from src.hmmer.Hmmer import Hmmer
-
-from src.common.definitions import ROOT_DIR
 
 
 class digIS:
 
-    def __init__(self, model, sequence_file, output_dir, config):
-        self.model = model
+    def __init__(self, sequence_file, output_dir, config, genbank_file=None):
         self.sequence = sequence_file
+        self.genbank_file = genbank_file
         self.sequence_len = get_seqlen(self.sequence)
         self.config = config
         self.genome = os.path.splitext(os.path.basename(sequence_file))[0]
         self.database = os.path.splitext(self.sequence)[0] + ".pep"
         translate_dna_seq_biopython(sequence=self.sequence, outseq=self.database)
-        self.hmm = Hmmer(self.model, self.database)
+        self.hmm = Hmmer(config.models, self.database)
         self.recs = []
         self.merged_records_idx = []
         self.output_dir = output_dir
         self.hmmer_output = os.path.join(output_dir, self.genome + '.hmmer3')
         self.csv_output = os.path.join(output_dir, self.genome + '.csv')
         self.filter_log = []
+        self.genbank_overlap = []
+        self.matched_recs = []
+        self.fp = 0
+        self.fn = 0
 
     def search(self):
         self.hmm.run(tool="hmmsearch", outfile=self.hmmer_output, curated_models=True)
@@ -47,25 +50,24 @@ class digIS:
             self.recs.append(RecordDigIS.from_hmmer(hsp, sid, dna_range[0], dna_range[1], strand,
                                                     self.genome, "chr", self.sequence, self.sequence_len))
 
-    def merge(self, merge_distance=300):
+    def merge(self):
         """ Merging hits in particular distance """
         records_indexes = set(range(len(self.recs)))
 
         print("Number of records before merging: {}.".format(len(self.recs)))
-        merged_records_indexes = self.merge_records(records_indexes, merge_distance)
+        merged_records_indexes = self.merge_records(records_indexes)
         merged_records = [self.recs[rec_index] for rec_index in merged_records_indexes]
         self.recs = merged_records
         print("Number of records after merging: {}.".format(len(self.recs)))
-        # self.merged_records_idx = merged_records_indexes
 
-    def merge_records(self, records_indexes, merge_distance=300):
+    def merge_records(self, records_indexes):
 
-        merged_records_indexes = self.__merge_records_in_distance(records_indexes, merge_distance)
+        merged_records_indexes = self.__merge_records_in_distance(records_indexes)
         merged_records_indexes = self.__merge_overlapping_records(merged_records_indexes)
 
         return merged_records_indexes
 
-    def __merge_records_in_distance(self, records_indexes, merge_distance):
+    def __merge_records_in_distance(self, records_indexes):
 
         records_indexes_dc = deepcopy(records_indexes)
         for current_record_idx in copy(records_indexes_dc):
@@ -77,7 +79,7 @@ class digIS:
                     current_record = self.recs[current_record_idx]
                     other_record = self.recs[other_record_idx]
 
-                    if current_record.should_be_merged(other_record, merge_distance):
+                    if current_record.should_be_merged(other_record, self.config.max_merge_distance):
                         spol = current_record.start < other_record.start
                         qpol = current_record.qstart < other_record.qstart
 
@@ -104,6 +106,68 @@ class digIS:
                         self.filter_log.append("{} merged with overlapping element: {}".format(current_record, other_record))
         return records_indexes_dc
 
+    def classification(self):
+
+        # Extend hits with flank regions and get best Blast hits in original range
+        classifier = digISClassifier()
+        if self.recs:
+            orf_blast_hits = Blast.get_best_blast_hits_in_range(self.recs, BlastX,
+                                                                self.config.context_size_orf,
+                                                                self.config.isfinder_orf_db)
+            is_dna_blast_hits = Blast.get_best_blast_hits_in_range(self.recs, BlastN,
+                                                                   self.config.context_size_is,
+                                                                   self.config.isfinder_is_db)
+
+            if self.genbank_file:
+                genbank_recs = list(RecordGenbank(i, self.genome, "chr", self.sequence, self.sequence_len)
+                                    for i in read_gb(self.genbank_file))
+                genbank_recs = list(rec for rec in genbank_recs if rec.type not in ['source'])
+                self.__assigns_genbank_annotation_by_overlap(genbank_recs, ignore_strand=True)
+                ds_genbank_recs = self.__get_digis_genbank_map(genbank_recs)
+
+                for digis_hit, gb_rec, orf_blast_hit, is_blast_hit in zip(self.recs, ds_genbank_recs,
+                                                                          orf_blast_hits, is_dna_blast_hits):
+
+                    classifier.classify(digis_hit, gb_rec, orf_blast_hit, is_blast_hit)
+            else:
+                for digis_hit, orf_blast_hit, is_blast_hit in zip(self.recs, orf_blast_hits, is_dna_blast_hits):
+                    classifier.classify(digis_hit, None, orf_blast_hit, is_blast_hit)
+
+    def __assigns_genbank_annotation_by_overlap(self, genbank_records=None, ignore_strand=False):
+
+        unbinded_genbank_idx = set(list(range(len(genbank_records))))
+        unbinded_digis_idx = set(list(range(len(self.recs))))
+        match_idx = []
+
+        # For each genbank record
+        for gb_rec_idx, gb_rec in enumerate(genbank_records):
+            # For each digis hit record
+            max_overlap = 0
+            for digis_rec_idx, digis_rec in enumerate(self.recs):
+                # Test for overlap
+
+                overlap = gb_rec.get_overlap_length(digis_rec, ignore_strand)
+                if overlap > max_overlap:
+                    max_overlap = overlap
+                if overlap >= self.config.min_gb_overlap:
+                    match_idx.append((gb_rec_idx, digis_rec_idx))
+                    unbinded_genbank_idx.discard(gb_rec_idx)
+                    unbinded_digis_idx.discard(digis_rec_idx)
+            self.genbank_overlap.append(max_overlap)
+
+        self.matched_recs = match_idx
+        self.fn = len(unbinded_genbank_idx)
+        self.fp = len(unbinded_digis_idx)
+
+    def __get_digis_genbank_map(self, gb_recs=None):
+        tool_recs = [[]] * len(self.recs)
+        digis_match_idx = set(digis_idx for genbank_idx, digis_idx in self.matched_recs)
+        for idx in digis_match_idx:
+            for genbank_idx, digis_idx in self.matched_recs:
+                if digis_idx == idx:
+                    tool_recs[digis_idx] = tool_recs[digis_idx] + [gb_recs[genbank_idx]]
+        return tool_recs
+
     def export(self, filename=None):
         csv_row = []
         csv_output = filename if filename else self.csv_output
@@ -111,13 +175,15 @@ class digIS:
             csv_row.append([rec.qid, rec.sid, rec.qstart, rec.qend, rec.start, rec.end, rec.strand, rec.acc])
         write_csv(csv_row, csv_output, ["qid", "sid", "qstart", "qend", "sstart", "send", "strand", "acc"])
 
-    def run(self, search=True, export=True, debug=False):
+    def run(self, search=True, classification=True, export=True, debug=False):
         if search:
             self.search()
         self.parse()
         if debug:
             self.export(os.path.join(self.output_dir, self.genome + '_nonfilter.csv'))
         self.merge()
+        if classification:
+            self.classification()
         if export:
             self.export()
 
