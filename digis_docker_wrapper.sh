fab3bf3c37181f39f84ce96366f31c1a64dd1681
Janka Puterova <janka2012@gmail.com>
2019-05-11 14:29:26 +0200

    copying from guest to host added

--- digis_docker_wrapper.sh
+++ digis_docker_wrapper.sh
@@ -16,9 +16,11 @@ o) OUTPUTDIR=${OPTARG};;
 esac
 done
 
-DOCKER_DATA=/digis_data
-DOCKER_RUN="sudo docker run"
-DOCKER_IMAGE="digis"
+DOCKER_DATA="/digis_data"
+DOCKER_CONTAINER_NAME="digis_tool"
+DOCKER_RUN="sudo docker run --name ${DOCKER_CONTAINER_NAME}"
+DOCKER_IMAGE_NAME="digis"
+DOCKER_OUTPUTDIR="/digis_output"
 DOCKER_MOUNT=""
 DOCKER_PARAMS=""
 
@@ -32,7 +34,7 @@ if [ ! -z ${INPUT_FASTA} ]
 then
 	input_fasta_fullpath=$(echo $(cd $(dirname "$INPUT_FASTA") && pwd -P)/$(basename "$INPUT_FASTA"))
 	DOCKER_MOUNT+=" -v ${input_fasta_fullpath}:${DOCKER_DATA}/$(basename ${INPUT_FASTA})"
-	DOCKER_PARAMS+=" -i ${DOCKER_DATA}/${INPUT_FASTA}"
+	DOCKER_PARAMS+=" -i ${DOCKER_DATA}/$(basename $INPUT_FASTA)"
 else
 	echo "INPUT_FASTA is unset. This argument is mandatory."
 	exit 2
@@ -43,16 +45,27 @@ if [ ! -z ${GENBANK} ]
 then
 	genbank_fullpath=$(echo $(cd $(dirname "$GENBANK") && pwd -P)/$(basename "$GENBANK"))
 	DOCKER_MOUNT+=" -v ${genbank_fullpath}:${DOCKER_DATA}/$(basename ${GENBANK})"
-	DOCKER_PARAMS+=" -g ${DOCKER_DATA}/${GENBANK}"
+	DOCKER_PARAMS+=" -g ${DOCKER_DATA}/$(basename $GENBANK)"
 fi
 
-DOCKER_PARAMS+=" -f $FORMAT -o $OUTPUTDIR"
+# mounting output directory
+outputdir_fullpath=$(echo $(cd $(dirname "$OUTPUTDIR") && pwd -P)/$(basename "$OUTPUTDIR"))
+DOCKER_MOUNT+=" -v ${outputdir_fullpath}:${DOCKER_OUTPUTDIR}"
 
-INPUT_FASTA=$(basename ${INPUT_FASTA})
-GENBANK=$(basename ${GENBANK})
+# adding script params
+DOCKER_PARAMS+=" -f $FORMAT -o ${DOCKER_OUTPUTDIR}"
 
-DOCKER_CMD=$DOCKER_RUN$DOCKER_MOUNT" "$DOCKER_IMAGE$DOCKER_PARAMS
-echo "$DOCKER_CMD"
+# creating full command
+DOCKER_CMD=$DOCKER_RUN$DOCKER_MOUNT" "$DOCKER_IMAGE_NAME" python3 digIS_search.py"$DOCKER_PARAMS
 
-sudo docker cp $DOCKER_IMAGE:/digis_output $OUTPUTDIR
+# clean up in case something went wrong
+sudo docker container rm $DOCKER_CONTAINER_NAME
 
+# run full command
+eval $DOCKER_CMD
+
+# copy output from docker container into host
+sudo docker cp ${DOCKER_CONTAINER_NAME}:${DOCKER_OUTPUTDIR} ${outputdir_fullpath}
+
+# remove stopped container
+sudo docker container rm $DOCKER_CONTAINER_NAME
