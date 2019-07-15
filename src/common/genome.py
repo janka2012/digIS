415df28139846f07703bdd72eb38bf1de725ef69
Janka Puterova <janka2012@gmail.com>
2019-07-15 19:26:20 +0200

    Check if IDs in multifasta are unique

--- src/common/genome.py
+++ src/common/genome.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from collections import OrderedDict
+from collections import Counter, OrderedDict
 from Bio import SeqIO
 from ..common.sequence import translate_dna_seq_biopython
 
@@ -20,9 +20,20 @@ class Genome:
     @staticmethod
     def parse_genomes(fasta_file, translate=True, output_dir=None):
         genomes_dict = OrderedDict()
+        genome_ids = []
         if os.path.exists(fasta_file):
             for genome_rec in SeqIO.parse(fasta_file, "fasta"):
+                genome_ids.append(genome_rec.id)
                 genomes_dict[genome_rec.id] = Genome(genome_rec, translate, output_dir)
+
+            # check if there are duplicates in genome_ids
+            duplicated_ids = [item for item, count in Counter(genome_ids).items() if count > 1]
+
+            if duplicated_ids:
+                raise ValueError("Multifasta file contains following duplicated ids: {}.\n"
+                                 "Ids have to be unique.".format(duplicated_ids))
+            print(duplicated_ids)
+
         else:
             logging.error("Filename {} does not exist.".format(fasta_file))
             raise FileNotFoundError("{} file does not exist.".format(fasta_file))
