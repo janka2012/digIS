d14282e385d0060f9e96f4e545fc3b30feceb14a
Janka Puterova <janka2012@gmail.com>
2019-07-26 14:14:29 +0200

    orf_db defined

--- src/common/genome.py
+++ src/common/genome.py
@@ -13,8 +13,8 @@ class Genome:
         self.desc = genome_rec.description
         self.name = genome_rec.id
         self.length = len(genome_rec.seq)
+        self.orf_db = os.path.join(output_dir, "pep", self.name + ".pep")
         if translate:
-            self.orf_db = os.path.join(output_dir, "pep", self.name + ".pep")
             translate_dna_seq_biopython(seqrec=genome_rec, outseq=self.orf_db)
 
     @staticmethod
