088168de0a6b3e80d014c1210e29ca0f50cb3571
Janka Puterova <janka2012@gmail.com>
2019-07-31 22:48:23 +0200

    fixed problem with output_dir set to None

--- src/common/genome.py
+++ src/common/genome.py
@@ -13,7 +13,8 @@ class Genome:
         self.desc = genome_rec.description
         self.name = genome_rec.id
         self.length = len(genome_rec.seq)
-        self.orf_db = os.path.join(output_dir, "pep", self.name + ".pep")
+        if output_dir:
+            self.orf_db = os.path.join(output_dir, "pep", self.name + ".pep")
         if translate:
             translate_dna_seq_biopython(seqrec=genome_rec, outseq=self.orf_db)
 
