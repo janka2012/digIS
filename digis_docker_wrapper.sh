aa0f30f93fe8b46d53185b745219f45758a79d96
Janka Puterova <janka2012@gmail.com>
2019-08-27 20:11:45 +0200

    small changes

--- digis_docker_wrapper.sh
+++ digis_docker_wrapper.sh
@@ -27,10 +27,12 @@ o) OUTPUTDIR=${OPTARG};;
 esac
 done
 
+CONTAINER_ID=$(cat /dev/urandom | tr -dc 'a-z0-9A-Z' | fold -w 8 | head -n 1)
 DOCKER_DATA="/digis_data"
-DOCKER_CONTAINER_NAME="digis_tool"
+DOCKER_CONTAINER_NAME="digis_${CONTAINER_ID}"
+echo $DOCKER_CONTAINER_NAME
 DOCKER_RUN="sudo docker run --name ${DOCKER_CONTAINER_NAME}"
-DOCKER_IMAGE_NAME="digis"
+DOCKER_IMAGE_NAME="janka2012/digis"
 DOCKER_OUTPUTDIR="/digis_output"
 DOCKER_MOUNT=""
 DOCKER_PARAMS=""
@@ -43,8 +45,8 @@ DOCKER_PARAMS=""
 if [ ! -z ${INPUT_FASTA} ]
 then
 	input_fasta_fullpath=$(echo $(cd $(dirname "$INPUT_FASTA") && pwd -P)/$(basename "$INPUT_FASTA"))
-	DOCKER_MOUNT+=" -v ${input_fasta_fullpath}:${DOCKER_DATA}/$(basename ${INPUT_FASTA})"
-	DOCKER_PARAMS+=" -i ${DOCKER_DATA}/$(basename $INPUT_FASTA)"
+	DOCKER_MOUNT="${DOCKER_MOUNT} -v ${input_fasta_fullpath}:${DOCKER_DATA}/$(basename ${INPUT_FASTA})"
+	DOCKER_PARAMS="${DOCKER_PARANS} -i ${DOCKER_DATA}/$(basename $INPUT_FASTA)"
 else
 	echo "INPUT_FASTA is unset. This argument is mandatory."
 	exit 2
@@ -54,16 +56,16 @@ fi
 if [ ! -z ${GENBANK} ]
 then
 	genbank_fullpath=$(echo $(cd $(dirname "$GENBANK") && pwd -P)/$(basename "$GENBANK"))
-	DOCKER_MOUNT+=" -v ${genbank_fullpath}:${DOCKER_DATA}/$(basename ${GENBANK})"
-	DOCKER_PARAMS+=" -g ${DOCKER_DATA}/$(basename ${GENBANK})"
+	DOCKER_MOUNT="${DOCKER_MOUNT} -v ${genbank_fullpath}:${DOCKER_DATA}/$(basename ${GENBANK})"
+	DOCKER_PARAMS="${DOCKER_PARAMS} -g ${DOCKER_DATA}/$(basename ${GENBANK})"
 fi
 
 # mounting output directory
 outputdir_fullpath=$(echo $(cd $(dirname "$OUTPUTDIR") && pwd -P)/$(basename "$OUTPUTDIR"))
-DOCKER_MOUNT+=" -v ${outputdir_fullpath}:${DOCKER_OUTPUTDIR}"
+DOCKER_MOUNT="${DOCKER_MOUNT} -v ${outputdir_fullpath}:${DOCKER_OUTPUTDIR}"
 
 # adding script params
-DOCKER_PARAMS+=" -o ${DOCKER_OUTPUTDIR}"
+DOCKER_PARAMS="${DOCKER_PARAMS} -o ${DOCKER_OUTPUTDIR}"
 
 # creating full command
 DOCKER_CMD=$DOCKER_RUN$DOCKER_MOUNT" "$DOCKER_IMAGE_NAME" python3 digIS_search.py"$DOCKER_PARAMS
