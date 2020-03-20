bd0e4a3f3dd39a73a0975ca6640ccd069f8adae0
Martinek Tomas <martinto@edesign1.fit.vutbr.cz>
2020-03-20 09:35:25 +0100

    [BUGFIX] ORF classification done against database of all ORFs

--- definitions.py
+++ definitions.py
@@ -3,7 +3,7 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 HMM_MODELS = os.path.join(ROOT_DIR, "data", "models", "hmm", "hmm_all_subfams.hmm")
 OUTLIERS = os.path.join(ROOT_DIR, "data", "models", "fasta", "outliers.fasta")
-ISFINDER_ORF_DB = os.path.join(ROOT_DIR, "data", "Blast_db", "isfinder_orf")
+ISFINDER_ORF_DB = os.path.join(ROOT_DIR, "data", "Blast_db", "isfinder_orf_all")
 ISFINDER_IS_DB = os.path.join(ROOT_DIR, "data", "Blast_db", "isfinder_is")
 CONTEXT_SIZE_ORF = 1600
 CONTEXT_SIZE_IS = 14000
